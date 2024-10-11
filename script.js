d119b07e7d63796ada742a4fcce0b0b7f3ba38d1
Anant Kumar LNU <kumar.anant@northeastern.edu>
2024-10-11 15:21:20 -0400

    live alert impl

--- script.js
+++ script.js
@@ -28,6 +28,27 @@ window.addEventListener('load', function() {
     }, 1500); // 1.5 seconds
 });
 
+// live alert
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Great to see your Interest, Please Signup to Begin the Journey ! ', 'success')
+  })
+}
+
 function validateFormControls(event){
     var value = event.target.value;
     var targetId = event.target.id;
